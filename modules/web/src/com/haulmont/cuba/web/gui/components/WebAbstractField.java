d0655168bd54fd6a8affdef529e8c17bcd58c49b
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-06-18 20:01:59 +0400

    PL-9286 New presentation data layer (initial commit)

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebAbstractField.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebAbstractField.java
@@ -38,7 +38,9 @@ import com.haulmont.cuba.gui.components.validators.BeanValidator;
 import com.haulmont.cuba.gui.data.Datasource;
 import com.haulmont.cuba.gui.data.ValueListener;
 import com.haulmont.cuba.gui.data.impl.WeakItemChangeListener;
+import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.web.gui.data.ItemWrapper;
+import com.haulmont.cuba.web.gui.model.ItemAdapter;
 import com.vaadin.ui.Component.HasContextHelp.ContextHelpIconClickListener;
 import org.apache.commons.lang.StringUtils;
 
@@ -54,10 +56,11 @@ import java.util.function.Consumer;
 import static com.haulmont.cuba.gui.ComponentsHelper.handleFilteredAttributes;
 
 public abstract class WebAbstractField<T extends com.vaadin.v7.ui.AbstractField>
-        extends WebAbstractComponent<T> implements Field {
+        extends WebAbstractComponent<T> implements Field, PropertyBoundComponent /* todo ds: move to Field */ {
 
     protected static final int VALIDATORS_LIST_INITIAL_CAPACITY = 4;
 
+    protected InstanceContainer instanceContainer;
     protected Datasource<Entity> datasource;
     protected MetaProperty metaProperty;
     protected MetaPropertyPath metaPropertyPath;
@@ -69,6 +72,7 @@ public abstract class WebAbstractField<T extends com.vaadin.v7.ui.AbstractField>
     protected boolean editable = true;
 
     protected ItemWrapper itemWrapper;
+    protected ItemAdapter itemAdapter;
 
     protected Datasource.ItemChangeListener<Entity> securityItemChangeListener;
     protected WeakItemChangeListener securityWeakItemChangeListener;
@@ -487,5 +491,68 @@ public abstract class WebAbstractField<T extends com.vaadin.v7.ui.AbstractField>
         if (contextHelpIconClickHandler != null) {
             contextHelpIconClickHandler.accept(event);
         }
+    }
+
+
+
+    @Override
+    public InstanceContainer getEntityContainer() {
+        return instanceContainer;
+    }
+
+    @Override
+    public void setContainer(InstanceContainer container, String property) {
+        if (this.instanceContainer != null) {
+            metaProperty = null;
+            metaPropertyPath = null;
+
+            component.setPropertyDataSource(null);
+
+            // todo dc
+//            //noinspection unchecked
+//            this.entityContainer.removeItemChangeListener(securityWeakItemChangeListener);
+//            securityWeakItemChangeListener = null;
+
+            this.instanceContainer = null;
+
+            if (itemAdapter != null) {
+                itemAdapter.unsubscribe();
+            }
+
+            disableBeanValidator();
+        }
+
+        if (container != null) {
+            //noinspection unchecked
+            this.instanceContainer = container;
+
+            final MetaClass metaClass = container.getMetaClass();
+            resolveMetaPropertyPath(metaClass, property);
+
+            initFieldConverter();
+
+            itemAdapter = createItemAdapter(container, Collections.singleton(metaPropertyPath));
+            component.setPropertyDataSource(itemAdapter.getItemProperty(metaPropertyPath));
+
+            initRequired(metaPropertyPath);
+
+            if (metaProperty.isReadOnly()) {
+                setEditable(false);
+            }
+
+            // todo dc
+//            handleFilteredAttributes(this, this.datasource, metaPropertyPath);
+//            securityItemChangeListener = e -> handleFilteredAttributes(this, this.datasource, metaPropertyPath);
+//
+//            securityWeakItemChangeListener = new WeakItemChangeListener(datasource, securityItemChangeListener);
+//            //noinspection unchecked
+//            this.datasource.addItemChangeListener(securityWeakItemChangeListener);
+
+            initBeanValidator();
+        }
+    }
+
+    protected ItemAdapter createItemAdapter(InstanceContainer container, Collection<MetaPropertyPath> propertyPaths) {
+        return new ItemAdapter(container, container.getMetaClass(), propertyPaths);
     }
 }
